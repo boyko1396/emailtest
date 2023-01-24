5a052b06389f1dde7af20ff6279b59abfb621238
Vasya <boyko1396@gmail.com>
2023-01-24 19:03:55 +0200

    v. 1

---
+++ build/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  triggerCopy();
+
+  $('.aside__slide:first').show();
+  $('.js-step-next').click(function() {
+    var currentSlide = $(this).parents('.aside__slide');
+    currentSlide.slideToggle(450);
+    currentSlide.next().slideToggle(450);
+  });
+
+  $('.js-step-prev').click(function() {
+    var currentSlide = $(this).parents('.aside__slide');
+    currentSlide.slideToggle(450);
+    currentSlide.prev().slideToggle(450);
+  });
+
+  // copy el
+  function triggerCopy() {
+    $('.js-copy-trigger').click(function() {
+      var copyText = $(this).data('copy');
+      var copyTextCopied = $(this).data('copy-text');
+      var copyCurrent = $(this);
+
+      var $copyCopy = $(this);
+      var $temp = $('<input>');
+      $('body').append($temp);
+      $temp.val($copyCopy.text()).select();
+      document.execCommand('copy');
+      $temp.remove();
+
+      copyCurrent.addClass('is-copied');
+      setTimeout(function() {
+        copyCurrent.removeClass('is-copied');
+      }, 1500);
+    });
+  }
+});
