909c0149068d65a2d9731d70e93a410e2e917e33
Vasya <boyko1396@gmail.com>
2023-01-27 18:25:56 +0200

    fix and add photo, animation

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,7 @@
-setInterval(function() {
-  $('body').addClass('is-animation');
-}, 550);
-
 $(document).ready(function() {
   triggerCopy();
+  parallaxInit();
+  animatedMainInit();
 
   $('.aside__slide:first').show();
   $('.js-step-next').click(function() {
@@ -39,3 +37,62 @@ $(document).ready(function() {
     });
   }
 });
+
+// animation main page
+function animatedMainInit() {
+  setInterval(function() {
+    $('body').addClass('is-animation');
+  }, 550);
+}
+
+// parallax main page init
+function parallaxInit() {
+  var b = document.getElementsByTagName('BODY')[0];  
+
+  b.addEventListener('mousemove', function(event) {
+    parallaxed1(event);
+    parallaxed2(event);
+    parallaxed3(event);
+    parallaxed4(event);
+  });
+
+  function parallaxed1(e) {
+    var amountMovedX = (e.clientX * -.2 / 8);
+    var amountMovedY = (e.clientY * -.2 / 8);
+    var x = document.getElementsByClassName('js-parallax-init-1');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
+    }
+  }
+
+  function parallaxed2(e) {
+    var amountMovedX = (e.clientX * -.2 / 5);
+    var amountMovedY = (e.clientY * -.2 / 5);
+    var x = document.getElementsByClassName('js-parallax-init-2');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
+    }
+  }
+
+  function parallaxed3(e) {
+    var amountMovedX = (e.clientX * .25 / 6);
+    var amountMovedY = (e.clientY * .25 / 6);
+    var x = document.getElementsByClassName('js-parallax-init-3');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
+    }
+  }
+
+  function parallaxed4(e) {
+    var amountMovedX = (e.clientX * .1 / 6);
+    var amountMovedY = (e.clientY * .1 / 6);
+    var x = document.getElementsByClassName('js-parallax-init-4');
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
+    }
+  }
+}
