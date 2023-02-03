ccdb4cbc5b0c7bfe160f3d70f66cea65cc716dd5
Vasya <boyko1396@gmail.com>
2023-02-04 01:27:59 +0200

    add animated

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   triggerCopy();
-  parallaxInit();
   animatedMainInit();
 
   $('.aside__slide:first').show();
@@ -14,6 +13,21 @@ $(document).ready(function() {
     var currentSlide = $(this).parents('.aside__slide');
     currentSlide.slideToggle(450);
     currentSlide.prev().slideToggle(450);
+  });
+
+  $('.js-progress-hook').click(function() {
+    var currentSlide = $(this).parents('.aside__slide');
+    $('.js-progress').toggleClass('is-active');
+    setTimeout(function() {
+      $('body').addClass('is-animation');
+    }, 550);
+    setTimeout(function() {
+      $('.aside').removeClass('aside--theme-gray');
+      $('.aside').addClass('aside--theme-red');
+      currentSlide.next().slideToggle(450);
+      currentSlide.next().next().slideToggle(450);
+      $('.main__layout').addClass('is-show');
+    }, 8700);
   });
 
   // copy el
@@ -43,56 +57,4 @@ function animatedMainInit() {
   setInterval(function() {
     $('body').addClass('is-animation');
   }, 550);
-}
-
-// parallax main page init
-function parallaxInit() {
-  var b = document.getElementsByTagName('BODY')[0];  
-
-  b.addEventListener('mousemove', function(event) {
-    parallaxed1(event);
-    parallaxed2(event);
-    parallaxed3(event);
-    parallaxed4(event);
-  });
-
-  function parallaxed1(e) {
-    var amountMovedX = (e.clientX * -.2 / 8);
-    var amountMovedY = (e.clientY * -.2 / 8);
-    var x = document.getElementsByClassName('js-parallax-init-1');
-    var i;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
-    }
-  }
-
-  function parallaxed2(e) {
-    var amountMovedX = (e.clientX * -.2 / 5);
-    var amountMovedY = (e.clientY * -.2 / 5);
-    var x = document.getElementsByClassName('js-parallax-init-2');
-    var i;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
-    }
-  }
-
-  function parallaxed3(e) {
-    var amountMovedX = (e.clientX * .25 / 6);
-    var amountMovedY = (e.clientY * .25 / 6);
-    var x = document.getElementsByClassName('js-parallax-init-3');
-    var i;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
-    }
-  }
-
-  function parallaxed4(e) {
-    var amountMovedX = (e.clientX * .1 / 6);
-    var amountMovedY = (e.clientY * .1 / 6);
-    var x = document.getElementsByClassName('js-parallax-init-4');
-    var i;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.transform='translate(' + amountMovedX + 'px,' + amountMovedY + 'px)'
-    }
-  }
 }
